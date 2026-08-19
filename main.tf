5dc1dd4c8257fbfeb0804b21d84f7a66ef51ef91
AnupJacob <anupjacob2017@gmail.com>
2026-08-19 11:49:37 +0100

    Adding the security group

--- main.tf
+++ main.tf
@@ -18,3 +18,12 @@ resource "azurerm_resource_group" "az-resource-grp" {
     environment = "dev"
   }
 }
+
+resource "azurerm_network_security_group" "az_network_grp" {
+  location            = azurerm_resource_group.az-resource-grp.location
+  name                = "az_network_group"
+  resource_group_name = azurerm_resource_group.az-resource-grp.name
+  tags = {
+    environment = "dev"
+  }
+}
