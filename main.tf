3bf2b4fd35eb0f05a9609cb32a9266327aa8b4fd
AnupJacob <anupjacob2017@gmail.com>
2026-08-19 17:09:11 +0100

    Adding the instance/ virtual machine for the application

--- main.tf
+++ main.tf
@@ -28,3 +28,29 @@ resource "azurerm_public_ip" "az_public_ip" {
     environment = "dev"
   }
 }
+
+resource "azurerm_linux_virtual_machine" "az-linux-vm" {
+  admin_username        = "adminuser"
+  location              = azurerm_resource_group.az-resource-grp.location
+  name                  = "az-linux-vm"
+  network_interface_ids = [azurerm_network_interface.az-nic.id]
+  resource_group_name   = azurerm_resource_group.az-resource-grp.name
+  size                  = "Standard_B1s"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
