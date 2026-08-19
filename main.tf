d66c9c349ce1b5cf047148bb6d7bd26a7d7ce35f
AnupJacob <anupjacob2017@gmail.com>
2026-08-19 12:04:54 +0100

    updating the address_space for CIDR

--- main.tf
+++ main.tf
@@ -27,3 +27,10 @@ resource "azurerm_network_security_group" "az_network_grp" {
     environment = "dev"
   }
 }
+
+resource "azurerm_virtual_network" "az_virtual_nwk" {
+  address_space       = ["10.122.10.10/16"]
+  location            = azurerm_resource_group.az-resource-grp.location
+  name                = "az_virtual_network"
+  resource_group_name = azurerm_resource_group.az-resource-grp.name
+}
